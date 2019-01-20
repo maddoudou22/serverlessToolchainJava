0419e1f7cfe2993111433599139f68432e4b919a
user <you@example.com>
2019-01-20 12:02:02 +0100

    latest push from windows

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Coucou, %s!";
+    private static final String template = "Salut, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
